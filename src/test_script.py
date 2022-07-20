2a0bd45d45f28a1d08fbfa63636aee1c7fc7f6d9
Sciancisco <3080877-Sciancisco@users.noreply.gitlab.com>
2022-07-20 12:41:12 -0400

    added test script to help with physical debugging

---
+++ src/test_script.py
@@ -0,0 +1,28 @@
+from time import sleep
+from qira_controller import QiraController, Trampoline
+from config import QiraConfig
+
+qc = QiraController(
+    QiraConfig.EXE_PATH,
+    QiraConfig.WINDOW_TITLE,
+    QiraConfig.ADDRESS,
+    QiraConfig.WINDOW_SIZE,
+    QiraConfig.WINDOW_POSITION,
+    QiraConfig.TRAMPOLINE_SELECTOR_POSITION,
+    QiraConfig.TRAMPOLINE_1_POSITION,
+    QiraConfig.TRAMPOLINE_2_POSITION,
+    QiraConfig.TRAMPOLINE_12_POSITION,
+    QiraConfig.TRAMPOLINE_AUTO_POSITION,
+)
+
+def test_run(gc):
+    qc.ready()
+    qc.send_routine_meta('Testy', 'McTest')
+    qc.start()
+    sleep(10)
+    qc.routine()
+    sleep(15)
+    qc.review()
+    qc.ready()
+
+
