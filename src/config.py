346ce6ea9072c24222c0a7e0f23aedbf64870d65
Sciancisco <3080877-Sciancisco@users.noreply.gitlab.com>
2022-07-20 12:11:33 -0400

    added a central configuration file

---
+++ src/config.py
@@ -0,0 +1,21 @@
+
+class QiraController:
+
+    WINDOW_SIZE = (1600, 855)
+    WINDOW_POSITION = (0, 0)
+
+    TRAMPOLINE_SELECTOR_POSITION = (240, 200)  # positions on a 1600x900 TN panel
+    TRAMPOLINE_1_POSITION = (240, 225)
+    TRAMPOLINE_2_POSITION = (240, 245)
+    TRAMPOLINE_12_POSITION = (240, 260)
+    TRAMPOLINE_AUTO_POSITION = (240, 280)
+
+    ADDRESS = '127.0.0.1:8080'
+    EXE_PATH = 'C:/Program Files/Qira/Qira'
+    WINDOW_TITLE = 'Qira v2.1.0'
+
+
+class Server:
+
+    DAT_DIRECTORY = ''
+    VIDEO_DIRECTORY = ''
