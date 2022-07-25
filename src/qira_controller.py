211a67ffd083ea436890000846f7cc5b219d5975
Sciancisco <3080877-Sciancisco@users.noreply.gitlab.com>
2022-07-25 15:26:07 -0400

    added a way to detect state from Qira
    also reworked the controller

--- src/qira_controller.py
+++ src/qira_controller.py
@@ -10,10 +10,12 @@ import requests
 class State(Enum):
 
     CLOSED = -1
-    READY = 0
-    START = 1
-    ROUTINE = 2
-    REVIEW = 3
+    NOUSB = 0
+    READY = 1
+    START = 2
+    ROUTINE = 3
+    REVIEW = 4
+    DIAGNOSIS = 5
 
 
 class Trampoline(Enum):
@@ -45,91 +47,111 @@ class QiraController:
         self._exe_path = exe_path
         self._window_title = window_title
         self._address = address
+
         self._window_size = window_size
         self._window_position = window_position
+
         self._trampoline_selector_position = trampoline_selector_position
         self._trampoline_1_position = trampoline_1_position
         self._trampoline_2_position = trampoline_2_position
         self._trampoline_12_position = trampoline_12_position
         self._trampoline_auto_position = trampoline_auto_position
-        self._window = None
-        self._state = State.CLOSED
 
-    def _window_exists(self):
-        # not the cleanest having a 'getter' changing the state of the object
-        # but convenient to ensure that is window exists then self._window != None.
+        self._ready_state_position = ready_state_position
+        self._ready_state_color = ready_state_color
+        self._start_state_position = start_state_position
+        self._start_state_color = start_state_color
+        self._routine_state_position = routine_state_position
+        self._routine_state_color = routine_state_color
+        self._review_state_position = review_state_position
+        self._review_state_color = review_state_color
+        self._nousb_state_position = nousb_state_position
+        self._nousb_state_color = nousb_state_color
+        self._diagnosis_state_position = diagnosis_state_position
+        self._diagnosis_state_color = diagnosis_state_color
+
+    def _detect_window(self):
         if self._window_title in gw.getAllTitles():
-            self._window = gw.getWindowsWithTitle(self._window_title)[0]
-            return True
+            return gw.getWindowsWithTitle(self._window_title)[0]
         else:
-            self._window = None
-            self._state = State.CLOSED
-            return False
+            return None
 
     def _position_window(self):
-        if not self._window_exists():
+        if not window := self._detect_window():
             raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
 
-        self._window.moveTo(*self._window_position)
-        self._window.resizeTo(*self._window_size)
+        window.moveTo(*self._window_position)
+        window.resizeTo(*self._window_size)
 
     def _press_space(self):
-        if not self._window_exists():
+        if not window := self._detect_window():
             raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
 
-        self._window.activate()
+        window.activate()
         pyautogui.press(' ')
 
     def _detect_state(self):
-        if not self._window_exists():
-            raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
-        # TODO ?
-        raise NotImplementedError("Not implemented yet.")
+        if not window := self._detect_window():
+            return State.CLOSED
+
+        window.activate()
+        screenshot = pyautogui.screenshot()
+
+        if screenshot.getpixel(self._ready_state_position) == self._ready_state_color:
+            return State.READY
+        elif screenshot.getpixel(self._start_state_position) == self._start_state_color:
+            return State.START
+        elif screenshot.getpixel(self._routine_state_position) == self._routine_state_color:
+            return State.ROUTINE
+        elif screenshot.getpixel(self._review_state_position) == self._review_state_color:
+            return State.REVIEW
+        elif screenshot.getpixel(self._no_usb_state_position) == self._no_usb_state_color:
+            return State.NOUSB
+        elif screenshot.getpixel(self._diagnosis_state_position) == self._diagnosis_state_color:
+            return State.DIAGNOSIS
+        else:
+            raise QiraControllerError("Could not detect state.")
 
     def launch(self):
-        if not self._window_exists():  # grab window here if exists
+        if not window := self._detect_window():  # grab window here if exists
             subprocess.Popen(self._exe_path)
 
             t = 0
             timeout = 10
-            while not self._window_exists() and t < timeout:
+            while not window := self._detect_window() and t < timeout:
                 time.sleep(1)
                 t += 1
 
             if not t < timeout:
                 raise QiraControllerError(f"Could not acquire Qira window with title '{self._window_title}'.")
         else:
-            self._window.activate()
-            self._state = State.READY
+            window.activate()
 
     def close(self):
-        if self._window_exists():
-            self._window.close()
-            while self._window_title in gw.getAllTitles():  # TODO: possible infinite loop there is a pop up that can prevent closure
-                time.sleep(1)
+        if window := self._detect_window():
+            window.close()
 
-        self._state = State.CLOSED
-
-    def send_routine_meta(self, firstname, lastname):
-        if self._state == State.CLOSED:
+    def send_routine_meta(self, firstname, lastname, timestamp):
+        if state := self._detect_state() == State.CLOSED:
             raise QiraControllerError("Qira is not running.")
-        if not self._window_exists():
-            raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
 
-        data = {'firstname': firstname, 'lastname': lastname}
+        if state not in {State.READY, State.START, State.ROUTINE, State.REVIEW}:
+            raise QiraControllerError(f"Cannot send routine meta in state {state}."
+
+        data = {'firstname': firstname, 'lastname': lastname, 'timestamp': timestamp}
         response = requests.post(f'http://{self._address}/routinemeta', json=data)
         if not response.ok:
             raise QiraControllerError(f"An error occured while sending routine meta: {response.status_code} - {response.reason}")
 
     def select_trampoline(self, trampoline):
-        if not self._window_exists():
+        if not window := self._detect_window():
             raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
 
-        if self._state != State.READY:
+        if self._detect_state() != State.READY:
             raise QiraControllerError(f"Can only select trampoline in {State.READY}.")
 
         self._position_window()
-        self._window.activate()
+        window.activate()
 
         pyautogui.click(*self._trampoline_selector_position)
         time.sleep(.5)
@@ -143,59 +165,11 @@ class QiraController:
         elif trampoline == Trampoline.AUTO:
             pyautogui.click(*self._trampoline_auto_position)
         else:
-            raise QiraControllerError("Invalid argument: trampoline must be in enum Trampoline.")
+            raise TypeError(f"Invalid argument 'trampoline': must be in enum Trampoline not {trampoline}.")
 
-    #
-    # transition functions
-    #
-    def force_ready(self):
-        self._state = State.READY
+    def change_state(self):
+        self._press_space()
 
-    def ready(self):
-        if not self._window_exists():
-            raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
-
-        # set state READY
-        if self._state == State.READY:
-            self._window.activate()
-
-        elif self._state == State.REVIEW:
-            self._press_space()
-            self._state = State.READY
-
-        else:
-            raise QiraControllerError(f"Cannot transition from {self._state} to {State.READY}.")
-
-    def start(self):
-        if not self._window_exists():
-            raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
-
-        # set state START
-        if self._state == State.READY:
-            self._press_space()
-            self._state = State.START
-        else:
-            raise QiraControllerError(f"Cannot transition from {self._state} to {State.START}.")
-
-    def routine(self):
-        if not self._window_exists():
-            raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
-
-        # set state ROUTINE
-        if self._state == State.START:
-            self._press_space()
-            self._state = State.ROUTINE
-        else:
-            raise QiraControllerError(f"Cannot transition from {self._state} to {State.ROUTINE}.")
-
-    def review(self):
-        if not self._window_exists():
-            raise QiraControllerError(f"No Qira window found with title '{self._window_title}'.")
-
-        # set state REVIEW
-        if self._state == State.ROUTINE:
-            self._press_space()
-            self._state = State.REVIEW
-        else:
-            raise QiraControllerError(f"Cannot transition from {self._state} to {State.REVIEW}.")
+    def get_state(self):
+        return self._detect_state()
 
