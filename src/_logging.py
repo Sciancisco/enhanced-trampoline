37228c45d1e4ac0244f9f682a3f0cd266ddd4224
Sciancisco <3080877-Sciancisco@users.noreply.gitlab.com>
2022-08-03 15:36:57 -0400

    tweaked the logger so it behaves properly

--- src/_logging.py
+++ src/_logging.py
@@ -2,13 +2,14 @@ import logging
 import sys
 
 
-LEVEL = logging.DEBUG
+GLOBAL_DEFAULT_LEVEL = logging.DEBUG
 
 
-def get_console_logger(name, level=LEVEL):
-    logger = logging.getLogger(__name__)
+def get_console_logger(name, level=GLOBAL_DEFAULT_LEVEL):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
 
-    handler = logging.StreamHandler(sys.stdout)
+    handler = logging.StreamHandler()
     handler.setLevel(level)
 
     formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
