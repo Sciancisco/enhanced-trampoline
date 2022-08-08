2198013ef1838f749e8ba6afe166dac78390bf56
Sciancisco <3080877-Sciancisco@users.noreply.gitlab.com>
2022-08-08 15:30:20 -0400

    added time to log

--- src/_logging.py
+++ src/_logging.py
@@ -20,7 +20,7 @@ def get_console_logger(name, log_file=LOG_FILE, level=GLOBAL_LEVEL):
     handler = logging.StreamHandler()
     handler.setLevel(level)
 
-    formatter = logging.Formatter("%(name)s:%(levelname)s: %(message)s")
+    formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s: %(message)s")
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
