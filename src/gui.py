136c138a7316996561412231d20920d8e6ae8eba
Sciancisco <3080877-Sciancisco@users.noreply.gitlab.com>
2022-08-22 14:47:59 -0400

    logging in gui.py

--- src/gui.py
+++ src/gui.py
@@ -5,10 +5,12 @@ import tkinter.ttk as ttk
 
 import cv2
 
+from _logging import get_console_logger
 from config import CameraConfig, QiraConfig, ServerConfig
 from server import Server
 
 
+logger = get_console_logger(__name__)
 root = Tk()
 
 
@@ -17,6 +19,7 @@ server = None
 
 
 def start_server():
+    logger.info(f"Start server with camera set to '{use_cam}'.")
     global server
     if server is None:
         server = Server(
@@ -26,6 +29,7 @@ def start_server():
 
 
 def stop_server():
+    logger.info("Stop server.")
     global server
     if server is not None:
         server.stop()
@@ -40,11 +44,13 @@ def use_cam_toggle():
     else:
         btn_use_cam.config(text="Yes")
         use_cam = True
+    logger.info(f"Toggled camera to '{use_cam}'.")
 
 
 def kill_all():
     stop_server()
     root.destroy()
+    logger.info("Quitted GUI.")
 
 
 frame = ttk.Frame(root, padding=10)
@@ -59,4 +65,6 @@ btn_use_cam.grid(column=1, row=1)
 
 root.title("Remote Qira")
 root.protocol("WM_DELETE_WINDOW", kill_all)
+
+logger.info("Entered GUI.")
 root.mainloop()
