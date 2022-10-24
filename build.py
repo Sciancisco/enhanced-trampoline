1e4ddbd134f3211e14f2797bc6dc4aba18e2917b
Sciancisco <66979306+Sciancisco@users.noreply.github.com>
2022-10-24 10:33:06 -0400

    fixed help message and blacked

--- build.py
+++ build.py
@@ -2,7 +2,9 @@ import argparse
 import PyInstaller.__main__
 
 parser = argparse.ArgumentParser("Build the application.")
-parser.add_argument("--clean", action="store_true", help="build release package")
+parser.add_argument(
+    "--clean", action="store_true", help="clean PyInstaller cache and remove temporary files before building"
+)
 args = parser.parse_args()
 
 options = ["src/gui.py", "--name", "Remote Qira", "--windowed", "--onefile"]
